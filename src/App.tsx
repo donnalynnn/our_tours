b373675391a7caa1fde000c0f9b3c81a5828ab3d
Donnalyn <donnalynrrma@gmail.com>
2023-09-28 01:21:37 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import Loading from './loading';
+import Tours from './all-tours';
+import {data} from './data';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  // const [tours, setTours] = useState<any[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [tours, setTours] = useState<{data} | null | any>(null);
+  
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+
+    setLoading(true);
+    try {
+      const tours = {data};
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button
+            className='btn'
+            style={{ marginTop: '2rem' }}
+            onClick={() => fetchTours()}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
