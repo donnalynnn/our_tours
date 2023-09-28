846d734520426fb7b9efcaf2f533ab93e99f958b
Donnalyn Reroma <143490599+donnalynnn@users.noreply.github.com>
2023-09-28 09:43:09 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react';
 import Loading from './loading';
 import Tours from './all-tours';
-import {data} from './data';
+// import {data} from './data';
 
 function App() {
   const [loading, setLoading] = useState(true);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   // const [tours, setTours] = useState<any[]>([]);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [tours, setTours] = useState<{data} | null | any>(null);
+  // const [tours, setTours] = useState<{data} | null | any>(null);
+  const url='https://course-api.com/react-tours-project';
+  const [tours, setTours] = useState([]);
   
   const removeTour = (id) => {
     const newTours = tours.filter((tour) => tour.id !== id);
@@ -19,7 +21,8 @@ function App() {
 
     setLoading(true);
     try {
-      const tours = {data};
+      const response = await fetch(url);
+      const tours = await response.json();
       setLoading(false);
       setTours(tours);
     } catch (error) {
